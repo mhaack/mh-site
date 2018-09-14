59690d5bf1352bb1d50f4908e62d329dc43132a5
Markus Haack <mhaack@adobe.com>
2018-09-14 17:02:31 +0200

    Added imprint link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,53 @@
 import React from 'react'
+import Link from 'gatsby-link'
 
 const Footer = () => (
     <footer id="footer">
         <div className="inner">
             <ul className="icons">
-                <li><a href="https://twitter.com/mhaack" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-                <li><a href="https://www.instagram.com/mhaack" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
-                <li><a href="https://github.com/mhaack" className="icon fa-github"><span className="label">Github</span></a></li>
-                <li><a href="https://de.linkedin.com/in/markushaack" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
-                <li><a href="https://www.xing.com/profile/Markus_Haack" className="icon fa-xing"><span className="label">Xing</span></a></li>
-                <li><a href="mailto:mail@markus-haack.de" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
+                <li>
+                    <a href="https://twitter.com/mhaack" className="icon fa-twitter">
+                        <span className="label">Twitter</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/mhaack" className="icon fa-instagram">
+                        <span className="label">Instagram</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://github.com/mhaack" className="icon fa-github">
+                        <span className="label">Github</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://de.linkedin.com/in/markushaack" className="icon fa-linkedin">
+                        <span className="label">LinkedIn</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.xing.com/profile/Markus_Haack" className="icon fa-xing">
+                        <span className="label">Xing</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="mailto:mail@markus-haack.de" className="icon fa-envelope-o">
+                        <span className="label">Email</span>
+                    </a>
+                </li>
             </ul>
             <ul className="copyright">
-                <li>&copy; Markus Haack</li><li>Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+                <li>&copy; Markus Haack</li>
+                <li>
+                    Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a> &amp;{' '}
+                    <a href="http://html5up.net">HTML5 UP</a>
+                </li>
+                <li>
+                    <Link to="imprint">Impressum</Link>
+                </li>
             </ul>
         </div>
     </footer>
 )
-    
+
 export default Footer
