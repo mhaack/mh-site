be54dee3db20970ca6e332a9c054bccc26b9900c
Markus Haack <mhaack@adobe.com>
2018-09-21 15:06:43 +0200

    Add gatsby-plugin-manifest plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,42 +1,54 @@
 module.exports = {
-  siteMetadata: {
-    title: "Markus Haack // Software Engineer",
-    author: "Markus Haack",
-    description: "Software Engineer working at Adobe and proud dad of a wonderful daughter."
-  },
-  pathPrefix: '/',
-  plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/projects`,
-        name: "projects",
-      },
+    siteMetadata: {
+        title: 'Markus Haack // Software Engineer',
+        author: 'Markus Haack',
+        description: 'Software Engineer working at Adobe and proud dad of a wonderful daughter.',
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/assets/images`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
+    pathPrefix: '/',
+    plugins: [
+        {
+            resolve: `gatsby-source-filesystem`,
             options: {
-              maxWidth: 630,
+                path: `${__dirname}/src/projects`,
+                name: 'projects',
             },
-          },
-          "gatsby-remark-copy-linked-files",
-        ],
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sass`
-  ],
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/assets/images`,
+            },
+        },
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            maxWidth: 630,
+                        },
+                    },
+                    'gatsby-remark-copy-linked-files',
+                ],
+            },
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: 'Markus Haack Website',
+                short_name: 'Markus H.',
+                start_url: '/',
+                background_color: '#333333',
+                theme_color: '#49bf9d',
+                display: 'minimal-ui',
+                icon: 'src/assets/images/icon_hi_res_512.png',
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-sass`,
+    ],
 }
