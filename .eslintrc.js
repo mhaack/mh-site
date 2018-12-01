f62c173f5d56432702b9021465243a8e3a2794b7
mhaack <developer.mhaack@gmail.com>
2018-12-01 08:03:09 +0100

    Add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    env: {
+        browser: true
+    },
+    extends: 'eslint:recommended',
+    plugins: ['react'],
+    rules: {
+        // react plugin - options
+        'react/jsx-uses-react': 'error',
+        'react/jsx-uses-vars': 'error',
+        'react/prop-types': 'error'
+    },
+    parser: 'babel-eslint',
+    parserOptions: {
+        ecmaVersion: 8
+    }
+}
