469ec468170541ee1c3bb7a81a7e8ed227acb326
mhaack <markus.haack@gmail.com>
2021-11-17 10:56:11 +0100

    Add CMS config

--- .eleventy.js
+++ .eleventy.js
@@ -1,107 +1,87 @@
-const { DateTime } = require('luxon');
-const readingTime = require('eleventy-plugin-reading-time');
-const pluginRss = require('@11ty/eleventy-plugin-rss');
-const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
-const htmlmin = require('html-minifier');
-const fs = require('fs');
-const path = require('path');
+const { DateTime } = require('luxon')
+const readingTime = require('eleventy-plugin-reading-time')
+const pluginRss = require('@11ty/eleventy-plugin-rss')
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
+const htmlmin = require('html-minifier')
+const fs = require('fs')
+const path = require('path')
 
-const isDev = process.env.ELEVENTY_ENV === 'development';
-const isProd = process.env.ELEVENTY_ENV === 'production';
+const isDev = process.env.ELEVENTY_ENV === 'development'
+const isProd = process.env.ELEVENTY_ENV === 'production'
 
-const manifestPath = path.resolve(
-    __dirname,
-    'public',
-    'assets',
-    'manifest.json'
-);
+const manifestPath = path.resolve(__dirname, 'public', 'assets', 'manifest.json')
 
 const manifest = isDev
     ? {
           'main.js': '/assets/main.js',
-          'main.css': '/assets/main.css'
+          'main.css': '/assets/main.css',
       }
-    : JSON.parse(fs.readFileSync(manifestPath, { encoding: 'utf8' }));
+    : JSON.parse(fs.readFileSync(manifestPath, { encoding: 'utf8' }))
 
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPlugin(readingTime);
-    eleventyConfig.addPlugin(pluginRss);
-    eleventyConfig.addPlugin(syntaxHighlight);
+    eleventyConfig.addPlugin(readingTime)
+    eleventyConfig.addPlugin(pluginRss)
+    eleventyConfig.addPlugin(syntaxHighlight)
 
-    eleventyConfig.setDataDeepMerge(true);
+    eleventyConfig.setDataDeepMerge(true)
+
+    eleventyConfig.addPassthroughCopy({ 'src/images': 'images' })
+    eleventyConfig.addPassthroughCopy('admin')
     
-    eleventyConfig.addPassthroughCopy({ 'src/images': 'images' });
-    eleventyConfig.setBrowserSyncConfig({ files: [manifestPath] });
+    eleventyConfig.setBrowserSyncConfig({ files: [manifestPath] })
 
-    eleventyConfig.addLayoutAlias('base', 'layouts/base.njk');
+    eleventyConfig.addLayoutAlias('base', 'layouts/base.njk')
 
     eleventyConfig.addShortcode('bundledcss', function () {
-        return manifest['main.css']
-            ? `<link href="${manifest['main.css']}" rel="stylesheet" />`
-            : '';
-    });
+        return manifest['main.css'] ? `<link href="${manifest['main.css']}" rel="stylesheet" />` : ''
+    })
 
     eleventyConfig.addShortcode('bundledjs', function () {
-        return manifest['main.js']
-            ? `<script src="${manifest['main.js']}"></script>`
-            : '';
-    });
+        return manifest['main.js'] ? `<script src="${manifest['main.js']}"></script>` : ''
+    })
 
     // short codes
-    eleventyConfig.addShortcode(
-        'currentYear',
-        require('./lib/shortcodes/currentYear')
-    );
-    eleventyConfig.addShortcode(
-        'youtubeEmbed',
-        require('./lib/shortcodes/youtubeEmbed')
-    );
+    eleventyConfig.addShortcode('currentYear', require('./lib/shortcodes/currentYear'))
+    eleventyConfig.addShortcode('youtubeEmbed', require('./lib/shortcodes/youtubeEmbed'))
 
-        eleventyConfig.addShortcode(
-            'githubBadge',
-            require('./lib/shortcodes/githubBadge')
-        );
+    eleventyConfig.addShortcode('githubBadge', require('./lib/shortcodes/githubBadge'))
 
     eleventyConfig.addFilter('excerpt', (post) => {
-        const content = post.replace(/(<([^>]+)>)/gi, '');
-        return content.substr(0, content.lastIndexOf(' ', 200)) + '...';
-    });
+        const content = post.replace(/(<([^>]+)>)/gi, '')
+        return content.substr(0, content.lastIndexOf(' ', 200)) + '...'
+    })
 
     eleventyConfig.addFilter('readableDate', (dateObj) => {
-        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toLocaleString(DateTime.DATE_FULL);
-    });
+        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toLocaleString(DateTime.DATE_FULL)
+    })
 
     eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(
-            'yyyy-LL-dd'
-        );
-    });
+        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd')
+    })
 
     eleventyConfig.addFilter('dateToIso', (dateString) => {
-        return new Date(dateString).toISOString();
-    });
+        return new Date(dateString).toISOString()
+    })
 
     eleventyConfig.addFilter('head', (array, n) => {
         if (n < 0) {
-            return array.slice(n);
+            return array.slice(n)
         }
 
-        return array.slice(0, n);
-    });
+        return array.slice(0, n)
+    })
 
-    eleventyConfig.addFilter('category', (items, category) =>
-        items.filter((item) => item.data.category === category)
-    );
+    eleventyConfig.addFilter('category', (items, category) => items.filter((item) => item.data.category === category))
 
     eleventyConfig.addCollection('projects', function (collection) {
-        return collection.getFilteredByTags('home-assistant');
-    });
+        return collection.getFilteredByTags('home-assistant')
+    })
 
     eleventyConfig.addCollection('tagList', function (collection) {
-        let tagSet = new Set();
+        let tagSet = new Set()
         collection.getAll().forEach(function (item) {
             if ('tags' in item.data) {
-                let tags = item.data.tags;
+                let tags = item.data.tags
 
                 tags = tags.filter(function (item) {
                     switch (item) {
@@ -109,43 +89,43 @@ module.exports = function (eleventyConfig) {
                         case 'nav':
                         case 'post':
                         case 'posts':
-                            return false;
+                            return false
                     }
 
-                    return true;
-                });
+                    return true
+                })
 
                 for (const tag of tags) {
-                    tagSet.add(tag);
+                    tagSet.add(tag)
                 }
             }
-        });
+        })
 
-        return [...tagSet];
-    });
+        return [...tagSet]
+    })
 
     eleventyConfig.addFilter('pageTags', (tags) => {
-        const generalTags = ['all', 'nav', 'post', 'posts'];
+        const generalTags = ['all', 'nav', 'post', 'posts']
 
         return tags
             .toString()
             .split(',')
             .filter((tag) => {
-                return !generalTags.includes(tag);
-            });
-    });
+                return !generalTags.includes(tag)
+            })
+    })
 
     eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
         if (outputPath && outputPath.endsWith('.html') && isProd) {
             return htmlmin.minify(content, {
                 removeComments: true,
                 collapseWhitespace: true,
-                useShortDoctype: true
-            });
+                useShortDoctype: true,
+            })
         }
 
-        return content;
-    });
+        return content
+    })
 
     return {
         dir: {
@@ -154,7 +134,7 @@ module.exports = function (eleventyConfig) {
             passthroughFileCopy: true,
             templateFormats: ['html', 'njk', 'md'],
             htmlTemplateEngine: 'njk',
-            markdownTemplateEngine: 'njk'
-        }
-    };
-};
+            markdownTemplateEngine: 'njk',
+        },
+    }
+}
