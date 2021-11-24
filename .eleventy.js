6422a98973bfd644608d3d112cadf1234e2dba4f
Markus Haack <markus.haack@gmail.com>
2021-11-24 23:00:30 +0100

    Switch to self hosted icon font

--- .eleventy.js
+++ .eleventy.js
@@ -26,6 +26,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.setDataDeepMerge(true)
 
     eleventyConfig.addPassthroughCopy({ 'src/images': 'images' })
+    eleventyConfig.addPassthroughCopy('src/assets')
     eleventyConfig.addPassthroughCopy('admin')
 
     eleventyConfig.setBrowserSyncConfig({ files: [manifestPath] })
--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,64 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin')
 
-const isDev = process.env.ELEVENTY_ENV === 'development';
+const isDev = process.env.ELEVENTY_ENV === 'development'
 
-const baseFilename = isDev ? 'main' : 'main.[contenthash]';
+const baseFilename = isDev ? 'main' : 'main.[contenthash]'
 
 module.exports = {
-  entry: [
-    path.resolve(__dirname, 'src', 'js', 'main.js'),
-    path.resolve(__dirname, 'src', 'css', 'main.css'),
-  ],
-  output: {
-    path: path.resolve(__dirname, 'dist', 'assets'),
-    filename: `${baseFilename}.js`,
-  },
+    entry: [path.resolve(__dirname, 'src', 'js', 'main.js'), path.resolve(__dirname, 'src', 'css', 'main.css')],
+    output: {
+        path: path.resolve(__dirname, 'dist', 'assets'),
+        filename: `${baseFilename}.js`,
+    },
 
-  optimization: {
-    minimize: !isDev,
-    minimizer: [new TerserPlugin({ parallel: true })],
-  },
+    optimization: {
+        minimize: !isDev,
+        minimizer: [new TerserPlugin({ parallel: true })],
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env'],
+                        },
+                    },
+                ],
             },
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1,
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                        },
+                    },
+                    'postcss-loader',
+                ],
             },
-          },
-          'postcss-loader',
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                use: [
+                    {
+                        loader: 'url-loader?limit=100000',
+                    },
+                ],
+            },
         ],
-      },
+    },
+
+    plugins: [
+        new WebpackManifestPlugin({ publicPath: '/assets/' }),
+        new MiniCssExtractPlugin({ filename: `${baseFilename}.css` }),
     ],
-  },
-
-  plugins: [
-    new WebpackManifestPlugin({ publicPath: '/assets/' }),
-    new MiniCssExtractPlugin({ filename: `${baseFilename}.css` }),
-  ],
-};
+}
