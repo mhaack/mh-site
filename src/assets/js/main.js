6d585dde7e74ce8e6e6e7dce42cbc2b405ffda8c
Markus Haack <markus.haack@gmail.com>
2021-12-26 22:08:25 +0100

    Show scroll to top later

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,14 +1,12 @@
-(function(document) {
+(function (document) {
     const scrollToTopButton = document.getElementById('scrollToTop')
     if (0 === scrollToTopButton.length) {
-      return
+        return
     }
-  
+
     scrollToTopButton.addEventListener('click', () => window.scroll({ behavior: 'smooth', top: 0 }))
-  
+
     window.addEventListener('scroll', () => {
-      (100 <= window.scrollY)
-        ? scrollToTopButton.style.opacity = 1
-        : scrollToTopButton.style.opacity = 0
+        500 <= window.scrollY ? (scrollToTopButton.style.display = 'block') : (scrollToTopButton.style.display = 'none')
     })
-  })(document)
+})(document)
