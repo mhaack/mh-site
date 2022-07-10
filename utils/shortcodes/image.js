860578a27c1eb9d3d0312662120ad6261572ec77
Markus Haack <markus.haack@gmail.com>
2022-07-10 11:29:14 +0200

    Optimize image generation for local dev

--- utils/shortcodes/image.js
+++ utils/shortcodes/image.js
@@ -1,15 +1,16 @@
 const Image = require('@11ty/eleventy-img')
 const path = require('path')
+const isProd = process.env.ELEVENTY_ENV === 'production'
 
 module.exports = async (relativeSrc, alt = 'image', className, sizes = '(min-width: 1024px) 100vw, 50vw') => {
     const { dir: imgDir } = path.parse(relativeSrc)
     const fullSrc = path.join('src', relativeSrc)
-    const ext = fullSrc.substring(fullSrc.lastIndexOf(".") + 1);
-    const currentFormat = ext == "png" ? "png" : "jpeg";
+    const ext = fullSrc.substring(fullSrc.lastIndexOf('.') + 1)
+    const currentFormat = ext == 'png' ? 'png' : 'jpeg'
 
     const imageMetadata = await Image(fullSrc, {
         widths: [null, 500, 900, 1500],
-        formats: ['avif', 'webp', currentFormat],
+        formats: [isProd ? 'avif' : null, 'webp', currentFormat],
         outputDir: path.join('dist', imgDir),
         urlPath: imgDir,
         // filenameFormat: function (id, src, width, format, options) {
