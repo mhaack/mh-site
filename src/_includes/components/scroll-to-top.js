44ccf1c042fb883acf1b5b61334cf7d38adeca7e
Markus Haack <markus.haack@gmail.com>
2023-01-29 12:58:00 +0100

    fix: format docs

--- src/_includes/components/scroll-to-top.js
+++ src/_includes/components/scroll-to-top.js
@@ -1,10 +1,10 @@
-const scrollToTopButton = document.getElementById('scrollToTop')
+const scrollToTopButton = document.getElementById('scrollToTop');
 if (scrollToTopButton) {
-    scrollToTopButton.addEventListener('click', () => window.scroll({ behavior: 'smooth', top: 0 }))
+    scrollToTopButton.addEventListener('click', () => window.scroll({ behavior: 'smooth', top: 0 }));
 
     window.addEventListener('scroll', () => {
         500 <= window.scrollY
             ? scrollToTopButton.classList.remove('opacity-0')
-            : scrollToTopButton.classList.add('opacity-0')
-    })
+            : scrollToTopButton.classList.add('opacity-0');
+    });
 }
