a7895468062846246c182b99d8b5df6cff95ec9d
Markus Haack <markus.haack@gmail.com>
2023-08-03 22:14:03 +0200

    fix: analytics data

--- src/_data/analytics.js
+++ src/_data/analytics.js
@@ -5,7 +5,7 @@ module.exports = async function () {
 
   const plausibleUrl = new URL('https://plausible.io/api/v1/stats/breakdown');
   plausibleUrl.searchParams.append('site_id', 'markus-haack.com');
-  plausibleUrl.searchParams.append('period', '12mo');
+  plausibleUrl.searchParams.append('period', 'all');
   plausibleUrl.searchParams.append('property', 'event:page');
   plausibleUrl.searchParams.append('metrics', 'pageviews,visitors');
 
